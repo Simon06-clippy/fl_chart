6d54ee03028112cf93aaecfb2b5381003b6ce8b5
simon <simon@naxan-group.com>
2025-10-07 17:26:44 +0200

    refactor: update matrix transformations to use simplified translate and scale methods

--- lib/src/chart/base/custom_interactive_viewer.dart
+++ lib/src/chart/base/custom_interactive_viewer.dart
@@ -417,11 +417,10 @@ class _CustomInteractiveViewerState extends State<CustomInteractiveViewer>
     }
 
     final nextMatrix = matrix.clone()
-      ..translateByDouble(
+      ..translate(
         alignedTranslation.dx,
         alignedTranslation.dy,
         0,
-        1,
       );
 
     // Transform the viewport to determine where its four corners will be after
@@ -532,7 +531,7 @@ class _CustomInteractiveViewerState extends State<CustomInteractiveViewer>
     );
     final clampedScale = clampedTotalScale / currentScale;
     return matrix.clone()
-      ..scaleByDouble(clampedScale, clampedScale, clampedScale, 1);
+      ..scale(clampedScale, clampedScale, clampedScale);
   }
 
   // Returns true iff the given _GestureType is enabled.
@@ -1112,9 +1111,9 @@ Offset _getMatrixTranslation(Matrix4 matrix) {
 // the given amount.
 Quad _getAxisAlignedBoundingBoxWithRotation(Rect rect, double rotation) {
   final rotationMatrix = Matrix4.identity()
-    ..translateByDouble(rect.size.width / 2, rect.size.height / 2, 0, 1)
+    ..translate(rect.size.width / 2, rect.size.height / 2, 0)
     ..rotateZ(rotation)
-    ..translateByDouble(-rect.size.width / 2, -rect.size.height / 2, 0, 1);
+    ..translate(-rect.size.width / 2, -rect.size.height / 2, 0);
   final boundariesRotated = Quad.points(
     rotationMatrix.transform3(Vector3(rect.left, rect.top, 0)),
     rotationMatrix.transform3(Vector3(rect.right, rect.top, 0)),
